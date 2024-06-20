80a293fe24c9cc77b7bdd00b1381e3159161d466
Satyam26-09 <satyam.sharma.cse25@heritageit.edu.in>
2024-06-20 18:24:37 +0530

    fixing build error: no console

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': 'error',
+  },
 }
